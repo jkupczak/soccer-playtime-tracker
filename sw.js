1df5a1b96f0fbbd28bdb28e6efeffe408d1735f3
James Kupczak <jimmykup@gmail.com>
2025-09-27 18:47:28 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'soccer-tracker-cache-v1';
+const CACHE_NAME = 'soccer-tracker-cache-v2';
 const urlsToCache = ['/', '/index.html', '/manifest.json', '/sw.js'];
 
 self.addEventListener('install', event => {
